6391649767340811a07f7e41c382c9c6e9a9e6c5
renhongl <renhongl@ncs.com.sg>
2018-01-17 17:33:40 +0800

    update

---
+++ spider_get_icon_code.py
@@ -0,0 +1,61 @@
+
+
+from selenium import webdriver
+import time
+import json
+
+def run():
+    show_browser = False
+    chrome_options = webdriver.ChromeOptions()
+
+    if show_browser:
+        browser = webdriver.Chrome()
+    else:
+        chrome_options.add_argument('--headless')
+        browser = webdriver.Chrome(chrome_options=chrome_options)
+        
+
+    try:
+        browser.get('http://www.htmleaf.com/ziliaoku/qianduanjiaocheng/20141225979.html')
+        time.sleep(3)
+        trs = browser.find_elements_by_css_selector('.table-striped tr');
+        codeObjArr = []
+        codeLine = ''
+        codeHtml = ''
+        for tr in trs:
+            content = tr.text
+            strArr = content.split(' ')
+            if len(strArr) > 3:
+                code = strArr.pop()
+                display = strArr.pop()
+                strArr.pop()
+                temp = {
+                    'name': ' '.join(strArr).lower(),
+                    'display': display,
+                    'code': code
+                }
+                line = '.mr-' + '-'.join(strArr).lower() + '::before{content: "' + code.replace('&#x', '\\').replace(';', '') + '";}\n'
+                codeLine = codeLine + line
+                html_line = '<i class="mr-icon mr-'+ '-'.join(strArr).lower() +'"></i>\n'
+                codeHtml = codeHtml + html_line
+                codeObjArr.append(temp)
+        with open('./output/icon/icon.json','w', encoding='utf-8') as icon_json:
+            json.dump(codeObjArr, icon_json, ensure_ascii=False)
+            print('save json finish')
+
+        with open('./output/icon/icon.txt', 'w', encoding='utf-8') as icon_txt:
+            icon_txt.write(codeLine);
+            print('save txt finish')
+
+        with open('./output/icon/icon.html', 'w', encoding='utf-8') as icon_html:
+            icon_html.write(codeHtml)
+            print('save html finish')
+
+    except Exception as e:
+        print(e)
+    finally:
+        browser.close()
+
+
+if __name__ == '__main__':
+    run()
---
+++ spider_get_icon_code_new.py
@@ -0,0 +1,59 @@
+
+
+from selenium import webdriver
+import time
+import json
+from bs4 import BeautifulSoup
+
+
+urls = [
+    'https://unicode-table.com/en/sets/emoji/',
+    'https://unicode-table.com/en/sets/arrows-symbols/',
+    'https://unicode-table.com/en/sets/hearts-symbols/',
+    'https://unicode-table.com/en/sets/flowers-symbols/',
+    'https://unicode-table.com/en/sets/stars-symbols/'
+]
+
+def run(i):
+    global urls
+    show_browser = False
+    chrome_options = webdriver.ChromeOptions()
+
+    if show_browser:
+        browser = webdriver.Chrome()
+    else:
+        chrome_options.add_argument('--headless')
+        browser = webdriver.Chrome(chrome_options=chrome_options)
+
+    try:
+        browser.get(urls[i])
+        time.sleep(3)
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        titles = soup.select('.stitle')
+        codes = soup.select('.code')
+        html_codes = ''
+        css_codes = ''
+        for i in range(len(titles)):
+            title = titles[i].text.lower().replace(' ', '-')
+            code = codes[i].text.replace('U+', '\\')
+            html_code = '<i class="mr-icon mr-'+title+'"></i>\n'
+            css_code = '.mr-' + title + '::before{content:"' + code + '";}\n'
+            html_codes = html_codes + html_code
+            css_codes = css_codes + css_code
+        with open('./output/newIcon/icon_html.txt', 'a', encoding='utf-8') as icon_html:
+            icon_html.write(html_codes)
+
+        with open('./output/newIcon/icon_css.txt', 'a', encoding='utf-8') as icon_css:
+            icon_css.write(css_codes)
+
+        print('Save finish')
+
+    except Exception as e:
+        print(e)
+    finally:
+        browser.close()
+
+
+if __name__ == '__main__':
+    for i in range(len(urls)):
+        run(i)
