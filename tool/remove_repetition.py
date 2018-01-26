590c8e3cd790bd4661f1a88e8497de4a560acf74
renhongl <renhongl@ncs.com.sg>
2018-01-26 18:07:49 +0800

    update

--- tool/remove_repetition.py
+++ tool/remove_repetition.py
@@ -8,11 +8,16 @@ def main():
             if line == '':
                 break
             lines.append(line)
+        data = set(lines)
 
-    data = set(lines)
     with open('./input/html_out.txt', 'w', encoding='utf-8') as html_out:
         lines = ''
+        lineArr = []
         for item in data:
+            lineArr.append(item)
+        
+        lineArr.sort()
+        for item in lineArr:
             lines = lines + item
         
         html_out.write(lines)
